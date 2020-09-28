ac0fcc7667b3c732cc8abf91f55dd32ccef9ed22
v_weefhuang <v_weefhuang@tencent.com>
2020-09-28 16:39:28 +0800

    commit test

---
+++ leetcode/link_list/t117.java
@@ -0,0 +1,41 @@
+package link_list;
+
+/**
+ * Created by v_weefhuang on 2020/9/28 16:31.
+ * leetcode 117 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ *
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class t117 {
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    };
+
+    class Solution {
+        public Node connect(Node root) {
+            return null;
+        }
+    }
+
+}
